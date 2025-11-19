0d7390898274aa1eacfbd698b005c5c3ccb1f777
Vadim Nesmeyanov <Vadim.Nesmeyanov@kaspersky.com>
2025-11-19 14:13:34 +0300

    Fix tests for 02_10

--- 02_10/test/test_sort.cpp
+++ 02_10/test/test_sort.cpp
@@ -5,29 +5,27 @@ TEST(sort, test1)
 {
     std::vector<int> vec = {5, 2, 8, 1, 9, 3, 7, 4, 6, 0};
     sort(vec);
-    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
-
+    EXPECT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 }
 TEST(sort, test2)
 {
     std::vector<int> vec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     sort(vec);
-    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
+    EXPECT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 }
 
 TEST(sort, test3)
 {
     std::vector<int> vec = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     sort(vec);
-    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
-
+    EXPECT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 }
 
 TEST(sort, test4)
 {
     std::vector<int> vec = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
     sort(vec);
-    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
+    EXPECT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 }
 
 TEST(sort, test5)
@@ -41,20 +39,19 @@ TEST(sort, test5)
     }
 
     sort(vec);
-    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
+    EXPECT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 }
 
 TEST(sort, test6)
 {
     std::vector<int> vec = {1};
     sort(vec);
-    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
+    EXPECT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 }
 
 TEST(sort, test7)
 {
     std::vector<int> vec;
     sort(vec);
-    ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
-
+    EXPECT_TRUE(std::is_sorted(vec.begin(), vec.end()));
 }
